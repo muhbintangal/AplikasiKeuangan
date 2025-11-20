af77b99a97a483e251da116aeb0c87ac2e7d4c5b
anggunnkaguraa <anggunnpermatasaari1012@gmail.com>
2025-11-20 16:57:59 +0700

    test

--- src/aplikasikeuangan/AplikasiKeuangan.java
+++ src/aplikasikeuangan/AplikasiKeuangan.java
@@ -15,7 +15,7 @@ public class AplikasiKeuangan {
      */
     public static void main(String[] args) {
         
-        System.out.println("Anung berak");
+        System.out.println("Anung turu");
         // TODO code application logic here
     }
     
