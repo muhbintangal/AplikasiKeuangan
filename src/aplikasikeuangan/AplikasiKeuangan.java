6b547aa4cb43a7eb2b624fc895771aec35e3bd9e
MyBook Hype AMD <MyBook Hype AMD@HYPE>
2025-11-20 16:09:14 +0700

    jidun ganteng

--- src/aplikasikeuangan/AplikasiKeuangan.java
+++ src/aplikasikeuangan/AplikasiKeuangan.java
@@ -14,6 +14,8 @@ public class AplikasiKeuangan {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        System.out.println("Anung berak");
         // TODO code application logic here
     }
     
